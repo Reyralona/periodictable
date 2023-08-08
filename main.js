50835aca5823bd9fa3a49100aaf9c12d17e3dc9b
Reyralona <reypetrucci1@gmail.com>
2023-08-08 20:08:24 -0300

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const periodicTable = document.querySelector(".table")
+
 // gets table info
 
 var request = new XMLHttpRequest()
@@ -11,5 +13,29 @@ function findElementByName(array, key, value) {
     });
 };
 
-let element = findElementByName(data.elements, 'name', 'Hydrogen')
-console.log(element[0])
+function createElementDiv(element) {
+    let elObj = findElementByName(data.elements, 'name', element)
+    let ele = elObj[0]
+    let atomicnumber = ele.number
+    let atomicmass = ele.atomic_mass
+    let symbol = ele.symbol
+    let name = ele.name
+    let category = ele.category.replace(/ /g, "_")
+
+    let eleDiv = `
+    <div id=${name} class="${category} element flex-col flex-center">
+    <div class="flex-row element_info">
+        <div class="atomicnumber">${atomicnumber}</div>
+        <div class="atomicmass">${atomicmass}</div>
+    </div>
+    <div class="symbol">${symbol}</div>
+    <div class="name">${name}</div>
+    </div>
+    `
+    console.log(eleDiv)
+    periodicTable.insertAdjacentHTML("beforeend", eleDiv)
+}
+
+createElementDiv("Nitrogen")
+
+
